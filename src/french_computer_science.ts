3b676fb8c914ccfba362dfb45c0f4914a4f3fd4c
Amaury <amaury.mulcey.pro@gmail.com>
2026-02-10 12:08:45 +0100

    Add A4 size for h and w

---
+++ src/french_computer_science.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="h-[297mm] w-[210mm] bg-white p-8 mx-auto">
+    <h1 class="text-xl">Hello World!</h1>
+  </div>
+`
