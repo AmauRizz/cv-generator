527ff2007dd911eb3b0c3e9feff3d92c3b3cb7b8
Amaury <amaury.mulcey.pro@gmail.com>
2026-02-10 13:12:15 +0100

    Resume Structure

--- src/french_computer_science.ts
+++ src/french_computer_science.ts
@@ -1,7 +1,63 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="h-[297mm] w-[210mm] bg-white p-8 mx-auto">
-    <h1 class="text-xl">Hello World!</h1>
+  <div class="h-[297mm] w-[210mm] bg-white text-black-classic p-8 mx-auto space-y-12">
+    <section class="space-y-4">
+        <article class="">
+            <h1 class="text-5xl font-bold">MULCEY Amaury</h1>
+            <h2 class="text-2xl text-important font-bold">Étudiant en Informatique</h2>
+        </article>
+        
+        <article class="grid grid-cols-3">
+            <div>
+                <a href="mailto:contact@amaurymulcey.fr">contact@amaurymulcey.fr</a>
+            </div>
+            
+            <div>
+                <p>La Rochelle, France</p>
+            </div>
+        </article>
+    </section>
+
+    <section class="grid grid-cols-2 gap-4">
+        <div class="space-y-8">
+            <section class="space-y-2">
+                <h2 class="text-2xl font-bold">Objectif de vie</h2>
+                <hr class="border-2 rounded-full" />
+            </section>
+            
+            <section class="space-y-2">
+                <h2 class="text-2xl font-bold">Education</h2>
+                <hr class="border-2 rounded-full" />
+            </section>
+            
+            <section class="space-y-2">
+                <h2 class="text-2xl font-bold">Experience</h2>
+                <hr class="border-2 rounded-full" />
+            </section>
+            
+            <section class="space-y-2">
+                <h2 class="text-2xl font-bold">Langages</h2>
+                <hr class="border-2 rounded-full" />
+            </section>
+        </div>
+        
+        <div class="space-y-8">
+            <section class="space-y-2">
+                <h2 class="text-2xl font-bold">Forces</h2>
+                <hr class="border-2 rounded-full" />
+            </section>
+        
+            <section class="space-y-2">
+                <h2 class="text-2xl font-bold">Compétences</h2>
+                <hr class="border-2 rounded-full" />
+            </section>
+            
+            <section class="space-y-2">
+                <h2 class="text-2xl font-bold">Passions</h2>
+                <hr class="border-2 rounded-full" />
+            </section>
+        </div>
+    </section>
   </div>
 `
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        Icons(),
     ],
 })
